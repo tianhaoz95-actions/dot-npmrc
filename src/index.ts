07731db7e0856587da352d0ed521ffeac2c871a9
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-01 18:23:19 -0700

    feat: init action

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import * as core from '@actions/core';
+import path from 'path';
+import fs from 'fs';
+
+const clearFile = (filename: string): void => {
+    fs.writeFileSync(filename, '', { flag: 'w+' });
+}
+
+const addLine = (filename: string, content: string): void => {
+    fs.appendFileSync(filename, content);
+}
+
+const main = (): void => {
+    const root = core.getInput('root');
+    const org = core.getInput('org');
+    const registry = core.getInput('registry');
+    const token = core.getInput('token');
+    // TODO(tianhaoz95): change this to getBooleanInput once
+    // it becomes available.
+    const overwrite = core.getInput('overwrite') === 'true';
+    const filename = '.npmrc';
+    const fullPath = path.join(root, filename);
+    const includeRegistry: boolean = (org !== '' && registry !== '');
+    const includeAuth: boolean = (token !== '' && registry !== '');
+    if (overwrite) {
+        core.info('Will overwrite .npmrc instead of appending.');
+        clearFile(fullPath);
+    }
+    if (includeRegistry) {
+        core.info('Both org and registry are found. Will add the registry line.');
+        const registryLine: string = `@${org}:registry=https://${registry}`;
+        addLine(fullPath, registryLine);
+    }
+    if (includeAuth) {
+        core.info('Both registry and token are found. Will add auth line.');
+        const authLine: string = `//${registry}/:_authToken=${token}`;
+        addLine(fullPath, authLine);
+    }
+    core.info('Done!');
+}
+
+main();
